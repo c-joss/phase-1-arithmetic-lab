5427d2f627a04dbce1dc85bcce83994d4b4df6ad
Courtney Macgregor <cjoss90@hotmail.com>
2024-08-25 17:08:28 +1000

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+
+let random = Math.floor(Math.random() * 100) +1;
+
+
+let num3 = 12;
+let num4 = 8;
+let mod = num3 % num4;
+
+let max = Math.max(20);
+
+
+
+
+
+
+
